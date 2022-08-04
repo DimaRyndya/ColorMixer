7e8f96b26884f567a2699275dce82c944211ffd2
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-08-04 15:54:55 +0300

    Initial commit

---
+++ ColorMixer/ColorMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixerApp.swift
+//  ColorMixer
+//
+//  Created by Dmitriy Ryndya on 04.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
